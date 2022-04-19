cb992d28efa9837fb7f7aa211ec44ebfa7ea4809
Logan Thomas <logan.thomas@CO-VAN-MAC103.firmglobal.com>
2022-04-18 18:27:17 -0700

    completed all tranformations; position, scale, rotation, grouping

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -10,14 +10,34 @@ const scene = new THREE.Scene();
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-mesh.position.x = 0.7;
-mesh.position.y = -0.6;
-mesh.position.z = 1;
-scene.add(mesh);
+const group = new THREE.Group();
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 1;
+scene.add(group);
 
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+
+group.add(cube1);
+cube2.position.x = -2;
+group.add(cube2);
+cube3.position.x = 2;
+group.add(cube3);
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper(1);
+scene.add(axesHelper);
 /**
  * Sizes
  */
