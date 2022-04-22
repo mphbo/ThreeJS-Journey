077b836371f0dcff5d8c182a93ce90ee5b0c818e
Logan Thomas <logan.thomas@forsta.com>
2022-04-21 18:54:06 -0700

    implemented the OrbitControls

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -1,6 +1,19 @@
 import "./style.css";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import gsap from "gsap";
+
+// Cursor
+
+const cursor = {
+  x: 0,
+  y: 0,
+};
+window.addEventListener("mousemove", (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = -(event.clientY / sizes.height - 0.5);
+  console.log(cursor);
+});
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -36,12 +49,19 @@ const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100);
 //   0.1,
 //   100
 // );
-camera.position.x = 2;
-camera.position.y = 2;
-camera.position.z = 2;
+
+camera.position.z = 3;
+// camera.position.y = 2;
+// camera.position.z = 2;
 console.log(camera);
 camera.lookAt(mesh.position);
 scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+// controls.target.y = 2;
+// controls.update();
 
 /**
  * Renderer
@@ -57,7 +77,16 @@ const clock = new THREE.Clock();
 // Animation
 const gameLoop = () => {
   const elapsedTime = clock.getElapsedTime();
-  mesh.rotation.y = elapsedTime;
+  // mesh.rotation.y = elapsedTime;
+
+  // Update camera
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  // camera.position.y = cursor.y * 5;
+  // camera.lookAt(mesh.position);
+
+  // Update controls
+  controls.update();
 
   // Render
   renderer.render(scene, camera);
