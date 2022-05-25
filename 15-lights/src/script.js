336478fc9d58d4ff6ac756a234f28b04c04c2657
Logan Thomas <logan.thomas@forsta.com>
2022-05-24 22:11:27 -0700

    created all kinds of lights as well as helpers to see where those lights were originating from

--- 15-lights/src/script.js
+++ 15-lights/src/script.js
@@ -2,6 +2,7 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "lil-gui";
+import { RectAreaLightHelper } from "three/examples/jsm/helpers/RectAreaLightHelper.js";
 
 /**
  * Base
@@ -22,7 +23,55 @@ const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
 
 const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3);
+directionalLight.position.set(1, 0.25, 0);
 scene.add(directionalLight);
+
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3);
+scene.add(hemisphereLight);
+
+const pointLight = new THREE.PointLight(0xff9000, 0.5, 10, 2);
+pointLight.position.set(1, -0.5, 1);
+scene.add(pointLight);
+
+// Below are high cost lights
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 1, 1);
+rectAreaLight.position.set(-1.5, 0, 1.5);
+rectAreaLight.lookAt(new THREE.Vector3());
+scene.add(rectAreaLight);
+
+const spotLight = new THREE.SpotLight(0x78ff00, 1, 9, Math.PI * 0.1, 0.25, 1);
+spotLight.position.set(0, 2, 3);
+scene.add(spotLight);
+
+spotLight.target.position.x = -1.75;
+scene.add(spotLight.target);
+
+// Helpers
+const hemisphereLightHelper = new THREE.HemisphereLightHelper(
+  hemisphereLight,
+  0.2
+);
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(
+  directionalLight,
+  0.2
+);
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2);
+
+const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+
+const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight);
+
+// scene.add(hemisphereLightHelper);
+// scene.add(directionalLightHelper);
+// scene.add(pointLightHelper);
+// scene.add(spotLightHelper);
+// scene.add(rectAreaLightHelper);
+
+window.requestAnimationFrame(() => {
+  spotLightHelper.update();
+});
 
 gui.add(ambientLight, "intensity").min(0).max(1).step(0.01);
 /**
